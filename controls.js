8e4b132a287092f90360ae4035137505bd888123
Jawwaad Sabree <jawwaad.sabree01@gmail.com>
2014-02-15 20:52:25 -0500

    Basic logic working. Needs to be styled.

--- controls.js
+++ controls.js
@@ -2,26 +2,57 @@ $(document).ready(function(){
 
 var playerX = "X";
 var playerO = "O";
-
 var currentPlayer = "X";
 
+var winOption1 = new Array("#a1", "#a2", "#a3");
+var winOption2 = new Array("#b1", "#b2", "#b3");
+var winOption3 = new Array("#c1", "#c2", "#c3");
+var winOption4 = new Array("#a1", "#b1", "#c1");
+var winOption5 = new Array("#a2", "#b2", "#c2");
+var winOption6 = new Array("#a3", "#b3", "#c3");
+var winOption7 = new Array("#a1", "#b2", "#c3");
+var winOption8 = new Array("#a3", "#b2", "#c1");
+var bigWinOptions = new Array(winOption1, winOption2, winOption3, winOption4, winOption5, winOption6, winOption7, winOption8);
 
+var isGameOver = function(){
+	
+	for(var start = 0; start < bigWinOptions.length; start++){
+		var winOption = bigWinOptions[start];
+		if(($(winOption[0]).text() != "") && ($(winOption[0]).text() == $(winOption[1]).text()) && ($(winOption[1]).text() == $(winOption[2]).text())){
+			return true;
+		}	
+	} 
+
+	return false;
+
+}
 
 $("td").click(function(){
 	if(currentPlayer == playerX){
 		$(this).text(playerX);
-		currentPlayer = playerO;
+		
+		if(isGameOver()){
+			alert("Game Over! Player " + currentPlayer + " won!!");	
+		}
+		else{
+			currentPlayer = playerO;			
+			//alert(currentPlayer + "\'s turn");
+		}
+		
 	}
 	else{
 		$(this).text(playerO);
-		currentPlayer = playerX;
+
+		if(isGameOver()){
+			alert("Game Over! Player " + currentPlayer + " won!!");	
+		}
+		else{
+			currentPlayer = playerX;
+			//alert(currentPlayer + "\'s turn");
+		}
 	}
 
-	alert(currentPlayer + "\'s turn");
+
 });
-
-
-
-
 
 });
